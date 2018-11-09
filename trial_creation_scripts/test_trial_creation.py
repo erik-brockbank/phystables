fbc13f675bb3c01e9483474eb9c404256b3f4cd0
Lab Computer <evullab@edvullab1.ucsd.edu>
2018-11-08 16:47:13 -0800

    lab_demo
    Added some new trials and modified creation script (erikb)

--- trial_creation_scripts/test_trial_creation.py
+++ trial_creation_scripts/test_trial_creation.py
@@ -5,7 +5,7 @@ TRIAL_DIMS = [600, 600]
 
 def make_test_trial():
     # Name the trial we're making (this will be used when we set up the trial and when we save it)
-    name = "test_auto_script_lab_comp"
+    name = "test_auto_script_lab_comp_2"
 
     # Create a "RedGreenTrial" with the name above and dimensions specified by global TRIAL_DIMS variable
     test_trial = RedGreenTrial(name = name, dims = TRIAL_DIMS)
@@ -14,10 +14,10 @@ def make_test_trial():
     test_trial.add_wall(upperleft = [50, 50], lowerright = [60, 100])
 
     # Add a ball to our trial
-    test_trial.add_ball(initpos = [350, 300], initvel = [1,1])
+    test_trial.add_ball(initpos = [400, 300], initvel = [1,1])
 
     # Add red and green goals to our trial (we always need one of each!)
-    test_trial.add_goal(upperleft = [400, 400], lowerright = [410, 500], color = GREEN, onreturn = GREENGOAL)
+    test_trial.add_goal(upperleft = [500, 400], lowerright = [410, 500], color = GREEN, onreturn = GREENGOAL)
     test_trial.add_goal(upperleft = [200, 200], lowerright = [250, 210], color = RED, onreturn = REDGOAL)
 
     # Save our trial to a file so we can run it later
